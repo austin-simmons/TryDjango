be5a2ee6f63a91b58564868876e528614b8c85ab
Austin Simmons <austinS115@gmail.com>
2016-01-09 19:02:39 -0800

    up to part 30

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,14 @@
+from urllib.parse import quote_plus
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 from .forms import PostForm
 
 # Create your views here.
 def post_create(request):
-    form = PostForm(request.POST or None)
+    form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
@@ -20,25 +22,39 @@ def post_create(request):
 
 def post_detail(request, id):
     instance = get_object_or_404(Post, id=id)
+    share_string = quote_plus(instance.content)
     context = {
         'title': instance.title,
-        'instance': instance
+        'instance': instance,
+        'share_string': share_string
     }
     return render(request, 'post_detail.html', context)
 
 
 def post_list(request):
-    queryset = Post.objects.all()
+    queryset_list = Post.objects.all()
+    paginator = Paginator(queryset_list, 2)
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        queryset = paginator.page(page)
+
+    except PageNotAnInteger:
+        queryset = paginator.page(1)
+
+    except EmptyPage:
+        queryset = paginator.page(paginator.num_pages)
     context = {
         'object_list': queryset,
-        'title': 'List'
+        'title': 'List',
+        'page_request_var': page_request_var
     }
     return render(request, 'post_list.html', context)
 
 
 def post_update(request, id=None):
     instance = get_object_or_404(Post, id=id)
-    form = PostForm(request.POST or None, instance=instance)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
